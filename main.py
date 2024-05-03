35f5555782aedf593e5a77dd923b60ba0c1ce4e9
Breno Carvalho <150388299+brenoocodes@users.noreply.github.com>
2024-05-03 12:59:17 -0300

    Colocando o contador de dedos

--- main.py
+++ main.py
@@ -16,10 +16,21 @@ if webcam.isOpened():
             frame = cv2.flip(frame, 1)
             #converter o frame de BGR (padrão OpenCv) para RGB (padrão do MediaPipe)
             frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-            #processar o reconhecimentos de mãos
             lista_maos = maos.process(frameRGB)
             altura, largura, _ = frame.shape
+
+
+            # Desenhar linhas e legendar os pixels na tela
+            # for y in range(0, altura, 50):
+            #     cv2.line(frame, (0, y), (largura, y), (0, 255, 0), 1)  # Desenha linhas horizontais
+            #     for x in range(0, largura, 50):
+            #         cv2.line(frame, (x, 0), (x, altura), (0, 255, 0), 1)  # Desenha linhas verticais
+            #         cv2.putText(frame, f'({x}, {y})', (x + 5, y + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 255), 1)
+
+
+
+            #processar o reconhecimentos de mãos
+            pontos = []
             #se mãos detectadas no frame
             if lista_maos.multi_hand_landmarks:
                 for mao in lista_maos.multi_hand_landmarks:
@@ -28,7 +39,32 @@ if webcam.isOpened():
                     #podemos desenhar os numeros dos pontos também
                     for numero_ponto, coordenada_do_ponto in enumerate(mao.landmark):
                         cx, cy = int(coordenada_do_ponto.x * largura), int(coordenada_do_ponto.y * altura)
-                        cv2.putText(frame, f'{numero_ponto}', (cx, cy), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
+                        cv2.putText(frame, f'{numero_ponto}', (cx, cy), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 20, 20), 1)
+                        pontos.append((cx, cy, numero_ponto)) 
+
+
+                    #fazer o contador de dedos
+                    dedos = [8, 12, 16, 20]
+                    contador = 0
+                    if mao:
+                        #fazer a logica para o dedao funcionar tanto na mão esquerda ou direita
+                        #mão esquerda
+                        if pontos[0][0] < pontos[4][0]:
+                            if pontos[4][0] > pontos[3][0]:
+                                contador += 1
+                        #mão direta
+                        if pontos[0][0] > pontos[4][0]:                 
+                            if pontos[4][0] < pontos[3][0]:
+                                contador += 1
+                        
+                        #agora a lógica para os quatros dedos restante
+                        for posicao_dedos in dedos:
+                            #Como estamos nos dedos superiores estamos pegando a posição y do plano, atráves do [1] presente no segundo parênteses e como o plano é de cima para baixo  verificamos se o pontos em cima é menor do que o ponto em baixo, lembrando que o plano cresce para baixo
+                            #Ex: se ponto 8 estiver menor que 7 é sinal que ele está acima, então dedo está levantado, caso contrário dedo abaixado
+                            if pontos[posicao_dedos][1] < pontos[posicao_dedos - 1 ][1]:
+                                contador += 1
+                        cv2.putText(frame, str(contador), (10,100), cv2.FONT_HERSHEY_SIMPLEX, 3, (255,0,0), 3)
+
 
 
             cv2.imshow('WebCam', frame)
